9e3ea6a0b729881adda1d142b2ab7d5396662508
George Jopson <georgejopson@gmail.com>
2026-08-01 15:43:13 +0100

    Add basic campaign page elements

--- web/src/components/CampaignPage/CampaignPage.tsx
+++ web/src/components/CampaignPage/CampaignPage.tsx
@@ -1,9 +1,36 @@
+import React from "react";
+import styles from "./CampaignPage.module.css"
+import Button from "../Button";
+import {Collapsible} from "radix-ui";
+
+import {QRCodeSVG} from 'qrcode.react';
 
 function CampaignPage() {
+    const [currentCampaign, setCurrentCampaign] = React.useState();
+    const links = [
+        "https://en.wikipedia.org/wiki/Charles_Babbage",
+        "https://en.wikipedia.org/wiki/Ada_Lovelace",
+        "https://en.wikipedia.org/wiki/Aquilegia",
+        "https://en.wikipedia.org/wiki/Nectar_spur",
+        "https://en.wikipedia.org/wiki/Pollinator"
+    ]
     return (
-        <>
-            Campaign Page
-        </>
+        <div className={styles.wrapper}>
+            <div className={styles.campaignWrapper}>
+                <Collapsible.Root>
+                    <Collapsible.Trigger className={styles.campaignTitle}> Campaign 1</Collapsible.Trigger>
+                    <Collapsible.Content className={styles.campaignContent}>
+                        <div className={styles.qrCodesWrapper}>
+                            {links.map((link, i) => (
+                                    <QRCodeSVG value={link} size={140} key={i}/>
+                            ))}
+                            <button className={styles.generateNewQrButton}>+</button>
+                        </div>
+                    </Collapsible.Content>
+                </Collapsible.Root>
+            </div>
+
+        </div>
     )
 }
 
