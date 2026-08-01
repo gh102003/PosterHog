3f760bc5fc1e870889b86814b66f127d80b22def
George Jopson <georgejopson@gmail.com>
2026-08-01 18:13:36 +0100

    Add mobile styles for the NavBar

--- web/src/components/NavBar/NavBar.tsx
+++ web/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,10 @@
 import styles from "./NavBar.module.css";
 import {PAGE_NAMES, type PageName} from "../../constants.ts";
 
+import { Collapsible } from "radix-ui";
+import { Menu } from 'feather-icons-react';
+
+
 interface NavBarProps {
     activePage: PageName;
     changePage: (page: PageName) => void;
@@ -9,16 +13,32 @@ interface NavBarProps {
 function NavBar({activePage, changePage}: NavBarProps) {
     return (
         <div className={styles.navBarWrapper}>
-            <div className={styles.side}></div>
-            <div className={styles.btnGroup}>
-                {
-                    PAGE_NAMES.map((pageName) => (
-                        <button key={pageName} onClick={()=>{changePage(pageName)}} className={`${styles.navBtn} ${activePage===pageName && styles.activeBtn}`}>{pageName}</button>
+            <div className={styles.desktopNavBar}>
+                <div className={styles.side}></div>
+                <div className={styles.btnGroup}>
+                    {
+                        PAGE_NAMES.map((pageName) => (
+                            <button key={pageName} onClick={()=>{changePage(pageName)}} className={`${styles.navBtn} ${activePage===pageName && styles.activeBtn}`}>{pageName}</button>
 
-                    ))
-                }
+                        ))
+                    }
+                </div>
+                <div className={styles.side}></div>
             </div>
-            <div className={styles.side}></div>
+            <div className={styles.mobileNavBar}>
+                <Collapsible.Root>
+                    <Collapsible.Trigger className={styles.mobileMenuBtn}><Menu size={"36"} color={"white"}/></Collapsible.Trigger>
+                    <Collapsible.Content className={styles.mobileNavBarBtnGroup}>
+                        {
+                            PAGE_NAMES.map((pageName) => (
+                                <button key={pageName} onClick={()=>{changePage(pageName)}} className={`${styles.mobileNavBtn} ${activePage===pageName && styles.activeBtn}`}>{pageName}</button>
+
+                            ))
+                        }
+                    </Collapsible.Content>
+                </Collapsible.Root>
+            </div>
+
 
         </div>
     )
