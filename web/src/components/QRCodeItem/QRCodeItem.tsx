d7a59b6016a3244a56d67d374bd8e5a80d548c92
George Jopson <georgejopson@gmail.com>
2026-08-01 15:52:55 +0100

    Add ability to download QR codes

---
+++ web/src/components/QRCodeItem/QRCodeItem.tsx
@@ -0,0 +1,36 @@
+import { useRef } from 'react';
+import { QRCodeCanvas } from 'qrcode.react';
+
+interface QRCodeItemProps {
+    link: string;
+    campaign: string;
+    qrCodeId: string;
+}
+
+function QRCodeItem({ link, campaign, qrCodeId }:QRCodeItemProps) {
+    const canvasRef = useRef<HTMLDivElement>(null);
+
+    const handleDownload = () => {
+        const canvas = canvasRef.current?.querySelector('canvas');
+        if (!canvas) return;
+
+        const url = canvas.toDataURL('image/png');
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `qr-${campaign}-${qrCodeId}.png`;
+        a.click();
+    };
+
+    return (
+        <div>
+            <div ref={canvasRef}>
+                <QRCodeCanvas value={link} size={140} />
+            </div>
+            <button onClick={handleDownload}>
+                Download PNG
+            </button>
+        </div>
+    );
+}
+
+export default QRCodeItem;
