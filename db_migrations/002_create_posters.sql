f618a962883d861e74854e3b96b9521f5f6e5e2c
George Howarth <george@pyesmeadow.com>
2026-08-01 13:22:59 +0100

    Link redirects

--- db_migrations/002_create_posters.sql
+++ db_migrations/002_create_posters.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS posters;
 CREATE TABLE IF NOT EXISTS posters (
     poster_id bigint unique GENERATED ALWAYS AS IDENTITY,
     link_uuid uuid unique not null,
-    campaign_id bigint references campaigns(campaign_id),
+    campaign_id bigint references campaigns(campaign_id) not null,
     location_lat real,
     location_long real,
     location_photo bytea not null
--- server/src/db.ts
+++ server/src/db.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "../prisma/generated/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: "postgresql://postgress:password@localhost:5432/postgres"
+  connectionString: "postgresql://postgres:password@localhost:5432/postgres"
 });
 
 export const prisma = new PrismaClient({ adapter });
