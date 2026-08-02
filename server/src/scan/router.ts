bd6b78390e671f8d4904e99d167357147d5c806d
George Howarth <george@pyesmeadow.com>
2026-08-02 10:58:00 +0100

    Scans by campaign endpoint

--- server/src/scan/router.ts
+++ server/src/scan/router.ts
@@ -3,7 +3,7 @@ import { prisma } from '../db.ts';
 
 export const router = express.Router();
 
-router.get('/:linkUuid', async (req: express.Request<{linkUuid: string}>, res: express.Response) => {
+router.get('/:linkUuid', async (req: express.Request<{ linkUuid: string }>, res: express.Response) => {
     const link_uuid = req.params.linkUuid;
 
     let poster;
@@ -12,7 +12,7 @@ router.get('/:linkUuid', async (req: express.Request<{linkUuid: string}>, res: e
             where: { link_uuid },
             include: { campaigns: true }
         });
-    } catch(err) {
+    } catch (err) {
         console.error(err)
         return res.status(404).json({ error: "Poster not found" });
     }
@@ -24,6 +24,28 @@ router.get('/:linkUuid', async (req: express.Request<{linkUuid: string}>, res: e
     recordScan(poster.poster_id)
 
     return res.redirect(301, poster.campaigns.destination);
+});
+
+router.get("/byCampaign/:campaignId", async (req: express.Request<{ campaignId: string }>, res: express.Response) => {
+    let campaignId;
+
+    try {
+        campaignId = Number(req.params.campaignId);
+    } catch (err) {
+        console.error(err)
+        return res.status(400).json({ error: "Campaign id invalid" });
+    }
+
+    const posters = await prisma.posters.findMany({
+        where: { campaign_id: campaignId },
+        omit: {location_photo: true},
+        include: {
+            scans: true
+        }
+    })
+
+    return res.status(200).json({posters});
+
 });
 
 async function recordScan(posterId: number) {
