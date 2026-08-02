2b19aa9afbc9da1839c9ec332282bd6d144d8dd4
George Howarth <george@pyesmeadow.com>
2026-08-02 17:05:33 +0100

    PDF upload starter

--- web/src/components/CreateCampaign/CreateCampaign.tsx
+++ web/src/components/CreateCampaign/CreateCampaign.tsx
@@ -1,26 +1,46 @@
 import { Collapsible, Form } from "radix-ui";
 import styles from "./CreateCampaign.module.css"
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { PDFDocument } from "pdf-lib";
+import { QRCodeCanvas } from 'qrcode.react';
 
 type Props = {
     handleSubmit: (name: string, destination: string) => Promise<void>
 }
 
-export function CreateCampaign({handleSubmit}: Props) {
+export function CreateCampaign({ handleSubmit }: Props) {
 
     const [open, setOpen] = useState<boolean>(false);
+    const pdfPreviewRef = useRef<HTMLEmbedElement>(null);
+
+    async function updatePdfPreview(posterFile: File) {
+        if (!posterFile) {
+            return;
+        }
+
+        const pdfDoc = await PDFDocument.load(await posterFile.arrayBuffer());
+        const firstPage = pdfDoc.getPage(0)
+
+        const modifiedPdfBytes = await pdfDoc.save();
+
+        const modifiedPdfBlob = new Blob([new Uint8Array(modifiedPdfBytes)], { type: "application/pdf" });
+        const url = URL.createObjectURL(modifiedPdfBlob);
+        if (pdfPreviewRef.current) {
+            pdfPreviewRef.current.src = url;
+        }
+    }
 
     return (
         <Collapsible.Root open={open} onOpenChange={setOpen}>
             <Collapsible.Trigger className={styles.newPosterBtn}>Create new poster campaign</Collapsible.Trigger>
             <Collapsible.Content className={styles.content}>
                 <Form.Root className={styles.formRoot}
-                    onSubmit={(event) => {
+                    onSubmit={async (event) => {
                         const data = Object.fromEntries(new FormData(event.currentTarget));
 
                         // Submit form data and catch errors in the response
                         handleSubmit(data.name as string, data.destination as string)
-                            .then(() => {setOpen(false)});
+                            .then(() => { setOpen(false) });
                         // prevent default form submission
                         event.preventDefault();
                     }}
@@ -35,7 +55,7 @@ export function CreateCampaign({handleSubmit}: Props) {
                         >
                             <Form.Label className={styles.Label}>Name</Form.Label>
                             <Form.Message className={styles.Message} match="valueMissing">
-                                Please enter your email
+                                Please enter a name for the campaign
                             </Form.Message>
                             <Form.Message className={styles.Message} match="tooShort">
                                 Please provide a longer name
@@ -65,6 +85,28 @@ export function CreateCampaign({handleSubmit}: Props) {
                             <input className={styles.Input} type="url" required />
                         </Form.Control>
                     </Form.Field>
+                    <Form.Field className={styles.Field} name="poster">
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "baseline",
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <Form.Label className={styles.Label}>Poster</Form.Label>
+                            <Form.Message className={styles.Message} match="valueMissing">
+                                Enter a link to open when the QR code is scanned
+                            </Form.Message>
+                        </div>
+                        <Form.Control asChild>
+                            <input type="file" accept="application/pdf" onChange={async (e) => {
+                                const posterFile = e.target.files?.[0] as File;
+                                await updatePdfPreview(posterFile);
+                            }} />
+                        </Form.Control>
+                    </Form.Field>
+                    <embed ref={pdfPreviewRef} style={{ width: "100%", height: 500 }} type="application/pdf"
+                        title="PDF Preview" />
                     <Form.Submit asChild>
                         <button className={styles.Button} style={{ marginTop: 10 }}>
                             Create poster
